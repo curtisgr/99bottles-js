a8f8779f38c82beabd9d9b86ece83c1e12d6649e
Gregory Curtis <curtisgr@gmail.com>
2017-08-10 20:01:53 -0400

    js

---
+++ app/bottles.js
@@ -0,0 +1,14 @@
+exports.verse = function () {
+    return [
+        '99 bottles of beer on the wall, 99 bottles of beer.',
+        'Take one down and pass it around, 98 bottles of beer on the wall.',
+    ].join('\n');
+};
+
+exports.verses = function () {
+    return;
+};
+
+exports.song = function () {
+    return;
+};
